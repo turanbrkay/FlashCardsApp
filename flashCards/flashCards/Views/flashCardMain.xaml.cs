2306e5ecd0baf45ab5201ce93867cddfce0d1662
turanbrkay <firminoloji9@gmail.com>
2022-10-10 02:24:40 +0300

    v0.3

--- flashCards/flashCards/Views/flashCardMain.xaml.cs
+++ flashCards/flashCards/Views/flashCardMain.xaml.cs
@@ -1,75 +1,64 @@
-﻿using System;
+﻿using MLToolkit.Forms.SwipeCardView;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 
 using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.Xaml;
+using static Xamarin.Essentials.Permissions;
 
 namespace flashCards.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class flashCardMain : TabbedPage
-    { 
+    {
         public ObservableCollection<Profile> _Profiles = new ObservableCollection<Profile>();
         public flashCardMain()
         {
-            
+
             InitializeComponent();
-            
+
             CardBinding();
             BindingContext = this;
 
-            
+
+
         }
+
         public void CardBinding()
         {
-            _Profiles.Add(new Profile() { Name="berkay", Age="18", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "turgut", Age = "30", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "tugce  ", Age = "24", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "halime", Age = "52", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "selin", Age = "10", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "mine", Age = "54", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "berkay", Age = "18", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "turgut", Age = "30", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "tugce  ", Age = "24", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "halime", Age = "52", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "selin", Age = "10", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "mine", Age = "54", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "berkay", Age = "18", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "turgut", Age = "30", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "tugce  ", Age = "24", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "halime", Age = "52", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "selin", Age = "10", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "mine", Age = "54", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "berkay", Age = "18", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "turgut", Age = "30", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "tugce  ", Age = "24", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "halime", Age = "52", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "selin", Age = "10", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "mine", Age = "54", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "berkay", Age = "18", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "turgut", Age = "30", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "tugce  ", Age = "24", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "halime", Age = "52", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "selin", Age = "10", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "mine", Age = "54", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "berkay", Age = "18", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "turgut", Age = "30", Photo = "icon.png" });
-            _Profiles.Add(new Profile() { Name = "tugce  ", Age = "24", Photo = "icon.png" });
-           
+            _Profiles.Add(new Profile() { Name = "true", Age = "18", Photo = "icon.png", visible = "true" });
+            _Profiles.Add(new Profile() { Name = "true", Age = "18", Photo = "icon.png", visible = "true" });
+            _Profiles.Add(new Profile() { Name = "true", Age = "18", Photo = "icon.png", visible = "true" });
+            _Profiles.Add(new Profile() { Name = "true", Age = "18", Photo = "icon.png", visible = "true" });
+            _Profiles.Add(new Profile() { Name = "true", Age = "18", Photo = "icon.png", visible = "true" });
+            _Profiles.Add(new Profile() { Name = "true", Age = "18", Photo = "icon.png", visible = "true" });
+            _Profiles.Add(new Profile() { Name = "true", Age = "18", Photo = "icon.png", visible = "true" });
+            _Profiles.Add(new Profile() { Name = "true", Age = "18", Photo = "icon.png", visible = "true" });
+            _Profiles.Add(new Profile() { Name = "true", Age = "18", Photo = "icon.png", visible = "true" });
+            _Profiles.Add(new Profile() { Name = "true", Age = "18", Photo = "icon.png", visible = "true" });
+            _Profiles.Add(new Profile() { Name = "true", Age = "18", Photo = "icon.png", visible = "true" });
+            _Profiles.Add(new Profile() { Name = "true", Age = "18", Photo = "icon.png", visible = "true" });
+            _Profiles.Add(new Profile() { Name = "true", Age = "18", Photo = "icon.png", visible = "true" });
+            _Profiles.Add(new Profile() { Name = "true", Age = "18", Photo = "icon.png", visible = "true" });
+            _Profiles.Add(new Profile() { Name = "true", Age = "18", Photo = "icon.png", visible = "true" });
+            _Profiles.Add(new Profile() { Name = "true", Age = "18", Photo = "icon.png", visible = "true" });
+
         }
         public ObservableCollection<Profile> Profiles
         {
             get => _Profiles;
             set
             {
-                _Profiles = value;  
+                _Profiles = value;
             }
 
         }
@@ -79,11 +68,27 @@ namespace flashCards.Views
             public string Name { get; set; }
             public string Age { get; set; }
             public string Photo { get; set; }
+
+            public string visible { get; set; }
+
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
             Navigation.PushModalAsync(new myWords());
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var label1 = (Label)sender;
+            if (label1.Text == "myword")
+            {
+                label1.Text = "naberlan";
+            }
+            else
+            {
+                label1.Text = "myword";
+            }
         }
     }
 }
